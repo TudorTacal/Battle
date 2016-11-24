a3b8a541a8f57415fde8c029827dae8777c30238
TudorTacal <tudortacal@gmail.com>
2016-11-24 18:23:32 +0000

    Challegen 26, initialized the Game with 2 players as arguments

--- app.rb
+++ app.rb
@@ -10,21 +10,20 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1 = $game.player_1
-    @player_2 = $game.player_2
+    @game = $game
     erb :play
   end
 
   post '/battle-names' do
-    p params
-    $game = Game.new(Player.new(params[:player_1]), Player.new(params[:player_2]))
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/attack' do
-    @player_1 = $game.player_1
-    @player_2 = $game.player_2
-    $game.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     redirect '/play'
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,27 @@
 require 'game'
 
 describe Game do
-  subject(:game) {described_class.new(player1, player2)}
-  let(:player1) {double(:player1)}
-  let(:player2) {double(:player2)}
+
+  subject(:game) {described_class.new(player_1, player_2)}
+  let(:player_1) {double(:player_1, :name => "Tom")}
+  let(:player_2) {double(:player_2, :name => "Dan")}
+
+  describe "#player_1" do
+    it "returns the first player" do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe "#player_2" do
+    it "returns the second player" do
+        expect(game.player_2).to eq player_2
+    end
+  end
 
   context "when #attack" do
     it 'reduces player_2 points by 10' do
-      expect(player2).to receive(:receive_damage)
-      game.attack(player2)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 end

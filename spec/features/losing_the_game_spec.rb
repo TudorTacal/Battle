69628e368c8ef837701962c79eea75b9fa974b1a
ejatkin <ejatkin@gmail.com>
2016-11-25 15:19:37 +0000

    Ex 29 complete - losing and winning - all tests passing

---
+++ spec/features/losing_the_game_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+RSpec.feature "Losing the game" do
+  scenario "Player 2 reaches zero points and loses the game" do
+    sign_in_and_play
+    12.times { click_button("Attack!") }
+    expect(page).to have_content("Elizabeth - you lost the game!")
+  end
+end
--- spec/features/switching_turns_spec.rb
+++ spec/features/switching_turns_spec.rb
@@ -7,4 +7,16 @@ RSpec.feature "Switching turns" do
     expect(page).to have_content("Jack 50/60")
     expect(page).to have_content("Elizabeth 50/60")
   end
+
+  scenario "showing whose turn it is" do
+    sign_in_and_play
+    expect(page).to have_content("It is currently Jack's turn")
+  end
+
+  scenario "after player_1 presses the attack button the message showing whose turn it is changes" do
+    sign_in_and_play
+    click_button("Attack!")
+    expect(page).to have_content("It is currently Elizabeth's turn")
+  end
+
 end
